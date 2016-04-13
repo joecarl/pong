0e454dfaca58d292eebeaf1debed7c10c1b86270
Jose Carlos HR <joecarlhr@gmail.com>
2016-04-14 01:07:22 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <allegro5/allegro.h>
-
+//plotictar monal
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
