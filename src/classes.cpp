6345ad2c98a9376b72c6610ec18a92b8a06d3479
joecarl <joecarlhr@gmail.com>
2021-08-31 16:44:49 +0200

    logic completely isolated from drawing functions

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,10 +1,6 @@
 #include "classes.h"
 #include "utils.h"
 
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
 #include <math.h>
 #include <stdexcept>
 #include <iostream>
@@ -14,13 +10,13 @@
 
 using namespace std;
 
-PongGame::PongGame(float sc, ALLEGRO_FONT* font){
+PongGame::PongGame(float sc){
 
-	ball = new element(true, 0, "");
-	players[0] = new playerP(scale * 1, 50, font);
-	players[1] = new playerP(scale * (DEF_W-GROSOR/2), 50, font);
-	bonus[0] = new element(false, BONUS_LONG, LONG_DIR);
-	bonus[1] = new element(false, BONUS_BALL, BALL_DIR);
+	ball = new Ball(this);
+	players[0] = new PlayerP(scale * 1, 50);
+	players[1] = new PlayerP(scale * (DEF_W-GROSOR/2), 50);
+	bonus[0] = new Bonus(this, BONUS_LONG);
+	bonus[1] = new Bonus(this, BONUS_BALL);
 	this->numPlayers = 0;
 	this->scale = sc;
 
@@ -49,22 +45,22 @@ void PongGame::iniciarPunto(int first){
 		while(tvx == 0)
 			tvx = scale * 2 * (rand() % 3 - 1);
 	}
-	else tvx = (int)ball->GetvX();
+	else tvx = (int)ball->getvX();
 	players[0]->medlen = MEDLEN; 
 	players[1]->medlen = MEDLEN;
 	players[0]->bonus_ball = 0; players[1]->bonus_ball = 0;
-	ball->SetParameters(scale * INIX, scale * INIY, tvx, scale * 0.5 * (rand() % 3 - 1));
-	players[0]->SetY(scale * 40);
-	players[1]->SetY(scale * 160);
+	ball->setParameters(scale * INIX, scale * INIY, tvx, scale * 0.5 * (rand() % 3 - 1));
+	players[0]->setY(scale * 40);
+	players[1]->setY(scale * 160);
 
 	//printf("iniciado nuevo punto\n");
 	
 }
 
-void PongGame::giveScore(playerP* pl, int score){
+void PongGame::giveScore(PlayerP* pl, int score){
 
 	for(unsigned int i = 0; i < this->numPlayers; i++){
-		playerP* iterPl = this->players[i];
+		PlayerP* iterPl = this->players[i];
 		if(iterPl != pl){
 			iterPl->racha = 0;
 		}
@@ -74,7 +70,7 @@ void PongGame::giveScore(playerP* pl, int score){
 	pl->racha++;
 
 	if(pl->racha > 3){
-		pl->GiveBonus(BONUS_IMPA);
+		pl->giveBonus(BONUS_IMPA);
 	}
 
 }
@@ -89,17 +85,17 @@ void PongGame::processTick(bool* keys){
 	if(keys[ALLEGRO_KEY_G]) players[0]->medlen += 1;//DEBUG
 
 	if(this->numPlayers == 2){
-		players[1]->ControlMove(scale, keys[ALLEGRO_KEY_UP], keys[ALLEGRO_KEY_DOWN], keys[ALLEGRO_KEY_I], keys[ALLEGRO_KEY_K]);
-		players[0]->ControlMove(scale, keys[ALLEGRO_KEY_W], keys[ALLEGRO_KEY_S]);
+		players[1]->controlMove(scale, keys[ALLEGRO_KEY_UP], keys[ALLEGRO_KEY_DOWN], keys[ALLEGRO_KEY_I], keys[ALLEGRO_KEY_K]);
+		players[0]->controlMove(scale, keys[ALLEGRO_KEY_W], keys[ALLEGRO_KEY_S]);
 	} else {
-		players[0]->ControlMove(scale, keys[ALLEGRO_KEY_UP], keys[ALLEGRO_KEY_DOWN]);
-		players[1]->MoveIA(this->numPlayers, ball, scale);
+		players[0]->controlMove(scale, keys[ALLEGRO_KEY_UP], keys[ALLEGRO_KEY_DOWN]);
+		players[1]->moveIA(this->numPlayers, ball, scale);
 	}
 	
 	//COMPROBAMOS QUE LA BOLA ESTÁ FUERA
-	if(ball->GetX() > scale * (320 + 15) || ball->GetX() < scale * (-15) ){
+	if(ball->getX() > scale * (320 + 15) || ball->getX() < scale * (-15) ){
 
-		if(ball->GetX() < -15){
+		if(ball->getX() < -15){
 			this->giveScore(players[1], 1);
 		} else {
 			this->giveScore(players[0], 1);
@@ -122,17 +118,24 @@ void PongGame::processTick(bool* keys){
 
 	}
 
-	ball->Process(scale, this->numPlayers, players);
+	ball->process(scale, this->numPlayers, players);
+
+	for(int i = 0; i < 2; i++){
+		PlayerP* pl = this->players[i];
+		if(pl->bonus_ball > 0){
+			pl->bonus_ball -= 0.1;
+		}
+	}
 
 	for(int i = 0; i < 2; i++){
 
-		if( bonus[i]->GetStat() == 0 && bonus_time[i] > 1000 && random_ex(0, 10000) < 1.0 ){
-			bonus[i]->SetParameters(random_ex(100, 240), random_ex(70, 130), random_ex(1.5, 2.0, true), random_ex(1.5, 2.0, true));
+		if( bonus[i]->getStat() == 0 && bonus_time[i] > 1000 && random_ex(0, 10000) < 1.0 ){
+			bonus[i]->setParameters(random_ex(100, 240), random_ex(70, 130), random_ex(1.5, 2.0, true), random_ex(1.5, 2.0, true));
 			bonus_time[i] =- 1;
 		}
 
-		bonus[i]->Process(scale, this->numPlayers, players);
-		if(bonus[i]->GetStat() == 0 && bonus_time[i] == -1)
+		bonus[i]->process(scale, this->numPlayers, players);
+		if(bonus[i]->getStat() == 0 && bonus_time[i] == -1)
 			bonus_time[i] = 1;
 		if(bonus_time > 0)
 			bonus_time[i]++;
@@ -142,33 +145,19 @@ void PongGame::processTick(bool* keys){
 
 
 
+//-----------------------------------------------------------------------------
+//------------------------------- [ Element ] ---------------------------------
 
+Element::Element(PongGame *game){
 
-
-
-
-
-element::element(bool iscircle, int bonus, const char *filename){
+	this->game = game;
 
 	vX = vY = t = 0;
 	x = y = x00 = y00 = -100;
 
-	if(!iscircle){
-		cout << "Loading bitmap: " << filename << endl;
-		sprite = al_load_bitmap(filename);
-		al_convert_mask_to_alpha(sprite, ALPHA_COLOR);
-		if(!sprite){
-			throw std::runtime_error("error loading bitmap");
-		}
-	}
-
-	isCircle = iscircle;
-	bonus_type = bonus;
-	radius = RADIUS;
-
 }
 
-void element::Process(int scale, int plyrNum, playerP* players[]){
+void Element::process(int scale, int plyrNum, PlayerP* players[]){
 
 	if(stat){
 
@@ -176,16 +165,9 @@ void element::Process(int scale, int plyrNum, playerP* players[]){
 		y = y00 + vY * t;
 		t++;
 
-		if(isCircle){
-			if(players[0]->bonus_ball && !players[1]->bonus_ball)
-				radius = (scale * 320 - x) / 20 + 2;
-			else if(players[1]->bonus_ball && !players[0]->bonus_ball)
-				radius = x / 20 + 2;
-			else
-				radius= RADIUS;
-		}
+		this->preprocess();
 
-		ProcessColliding(scale, plyrNum, players);
+		this->processColliding(scale, plyrNum, players);
 		
 		if(x < -scale * 80 || x > scale * DEF_W + 50){
 			stat = 0;
@@ -195,25 +177,8 @@ void element::Process(int scale, int plyrNum, playerP* players[]){
 
 }
 
-void element::Draw(int scale){
 
-	if(!this->stat){
-		return;
-	}
-
-	if(isCircle){
-
-		al_draw_filled_circle(x, y, radius*scale, al_map_rgb( 255,255, 255));
-
-	} else {
-
-		al_draw_bitmap(sprite, x - 10, y - 10, 0);// x-sprite->w/2, y-sprite->h/2);
-
-	}
-
-}
-
-void element::SetParameters(float px, float py, float vx, float vy, int st){
+void Element::setParameters(float px, float py, float vx, float vy, int st){
 
 	x = x00 = px;
 	y = y00 = py;
@@ -224,31 +189,22 @@ void element::SetParameters(float px, float py, float vx, float vy, int st){
 
 }
 
-void element::ProcessColliding(int scale, int plyrNum, playerP* players[]){
+void Element::processColliding(int scale, int plyrNum, PlayerP* players[]){
 
 	//CHOQUE CON LA PARTE DE ARRIBA
 	if(y <= scale * (radius + LIMIT))
-		SetParameters(this->x, scale*(radius+LIMIT+1), this->vX, -this->vY, stat);
+		this->setParameters(this->x, scale*(radius+LIMIT+1), this->vX, -this->vY, stat);
 
 	//CHOQUE CON LA PARTE DE ABAJO
 	if(y >= scale * (MAX_Y - radius - LIMIT))
-		SetParameters(this->x, scale * (MAX_Y - radius - LIMIT - 1), this->vX, -this->vY, stat);
+		this->setParameters(this->x, scale * (MAX_Y - radius - LIMIT - 1), this->vX, -this->vY, stat);
 
 	//CHOQUE CON LA PALA IZDA
 	if(x <= scale * (radius + GROSOR)){
-		if(fabs(y-players[0]->GetY()) < scale * (players[0]->medlen+radius) && x > scale * (GROSOR)){
-			if(isCircle){
-				if(y - players[0]->GetY() > scale*2) vY += 1;
-				else if (y - players[0]->GetY() > scale*1) vY += 0.5;
-				if(y - players[0]->GetY() < -scale*2) vY -= 1;
-				if (y - players[0]->GetY() < -scale*1) vY -= 0.5;
-					PlaySound(Mi, 40, 4);
-					PlayAudio();
-				SetParameters(scale * (radius + GROSOR + 1), y, -vX, vY, stat);
-			} else {
-				stat = 0;
-				players[0]->GiveBonus(bonus_type);
-			}
+		if(fabs(y-players[0]->getY()) < scale * (players[0]->medlen + radius) && x > scale * (GROSOR)){
+
+			 this->playerHit(players[0]);
+			
 		}
 	}
 
@@ -259,37 +215,82 @@ void element::ProcessColliding(int scale, int plyrNum, playerP* players[]){
 
 	if(x >= scale * (320 - radius - grosorB)){
 
-		if(fabs(y - players[1]->GetY()) < scale * (players[1]->medlen + radius) && x < scale * (320 - grosorB)){
+		if(fabs(y - players[1]->getY()) < scale * (players[1]->medlen + radius) && x < scale * (320 - grosorB)){
 
-			if(isCircle){
-
-				if(plyrNum != 0){
-					if(y - players[1]->GetY() > scale * 2) vY += 1;
-					else if (y - players[1]->GetY() > scale * 1) vY += 0.5;
-					if(y - players[1]->GetY() < -scale * 2) vY -= 1;
-					if (y - players[1]->GetY() < -scale * 1) vY -= 0.5;
-					PlaySound(Mi, 40, 4);
-					PlayAudio();
-				}
-				SetParameters(scale*(320 - radius - grosorB - 1), y, -vX, vY, stat);
-
-			} else {
-
-				stat = 0;
-				players[1]->GiveBonus(bonus_type);
-			}
-
+			this->playerHit(players[1]);
+			
 		}
 
 	}
 
 }
 
+//-----------------------------------------------------------------------------
+//--------------------------------- [ Ball ] ----------------------------------
+
+Ball::Ball(PongGame *game): Element(game){
+	
+	this->radius = RADIUS;
+
+}
+
+void Ball::preprocess(){
+
+	if(this->game->players[0]->bonus_ball && !this->game->players[1]->bonus_ball)
+		radius = (320 - x) / 20 + 2;
+	else if(this->game->players[1]->bonus_ball && !this->game->players[0]->bonus_ball)
+		radius = x / 20 + 2;
+	else
+		radius = RADIUS;
+
+	radius *= this->game->scale;
+
+}
+
+void Ball::playerHit(PlayerP *pl){
+	
+	if(pl == this->game->players[0] || this->game->numPlayers != 0){
+
+		if(y - pl->getY() > this->game->scale * 2) vY += 1;
+		else if (y - pl->getY() > this->game->scale * 1) vY += 0.5;
+		if(y - pl->getY() < -this->game->scale * 2) vY -= 1;
+		if (y - pl->getY() < -this->game->scale * 1) vY -= 0.5;
+
+		PlaySound(Mi, 40, 4);
+		PlayAudio();
+
+	}
+
+	int grosorB;
+	if(this->game->numPlayers != 0) grosorB = GROSOR;
+	else grosorB = 0;
+
+	float newX = pl == this->game->players[0] ? (radius + GROSOR + 1) : (320 - radius - grosorB - 1);
+
+	this->setParameters(this->game->scale * newX, y, -vX, vY, stat);
+
+}
+
+
+//------------------------------------------------------------------------------
+//--------------------------------- [ Bonus ] ----------------------------------
+
+Bonus::Bonus(PongGame *game, int bonus_type): Element(game){
+	
+	this->bonus_type = bonus_type;
+
+}
+
+void Bonus::playerHit(PlayerP *pl){
+	
+	pl->giveBonus(this->bonus_type);
+
+}
+
 //------------------------------------------------------------------------------
 
-playerP::playerP(int px, int py, ALLEGRO_FONT* _font){
+PlayerP::PlayerP(int px, int py){
 
-	font = _font;
 	x = px;
 	y = py;
 	medlen = MEDLEN;
@@ -300,7 +301,7 @@ playerP::playerP(int px, int py, ALLEGRO_FONT* _font){
 
 }
 
-void playerP::LockLimit(int scale){
+void PlayerP::lockLimit(int scale){
 
 	if(y >= scale * (MAX_Y - medlen - LIMIT))
 		y = scale * (MAX_Y - medlen - LIMIT - 1);
@@ -309,70 +310,28 @@ void playerP::LockLimit(int scale){
 
 }
 
-void playerP::Draw(int scale){
-
-	int medln = this->medlen;
-	
-	al_draw_filled_rectangle(
-		x - 2, 
-		y - scale * medln, 
-		x + 2, 
-		y + scale * medln, 
-		WHITE
-	);
-	al_draw_filled_rectangle(
-		x - 1,
-		y - scale * medln - 1, 
-		x + 1, 
-		y + scale * medln+1, 
-		WHITE
-	);
-	 
-	static int bonus_ball_decrease = 10;
-
-	if(bonus_ball){
-		if(x < 100)	al_draw_filled_rectangle(0, 16, 158.0 * bonus_ball / 80.0, 20, al_map_rgb(0, 200, 50));
-		else		al_draw_filled_rectangle(DEF_W, 16, DEF_W - 158.0 * bonus_ball / 80.0, 20, al_map_rgb(0, 200, 50));
-		
-		bonus_ball_decrease--;
-		if (bonus_ball_decrease <= 0){
-			bonus_ball_decrease = 10;
-			bonus_ball--;
-		}
-	}
-
-	if(comTxtY >- 40){
-		int txtX;
-		if(x<100) txtX = scale * 60;
-		else txtX = scale * 220;
-		al_draw_text(font, al_map_rgb( 150-comTxtY,150-comTxtY*4, 255), txtX, comTxtY, ALLEGRO_ALIGN_CENTER, comTxt);//al_map_rgb(255,0,255) );
-		comTxtY -= 2;
-	}
-
-}
-
-void playerP::MoveIA(int plyrNum, element *ball,int scale){
+void PlayerP::moveIA(int plyrNum, Element *ball,int scale){
 
 	if(plyrNum == 1){
-		if(y > ball->GetY()) y -= scale * (1 + rand() % 2);
-		if(y < ball->GetY()) y += scale * (1 + rand() % 2);
+		if(y > ball->getY()) y -= scale * (1 + rand() % 2);
+		if(y < ball->getY()) y += scale * (1 + rand() % 2);
 	}
 	else if(plyrNum == 0)
-		y = ball->GetY();
+		y = ball->getY();
 
-	LockLimit(scale);
+	this->lockLimit(scale);
 
 }
 
-void playerP::ControlMove(int scale, int key_up, int key_down,int key_up2, int key_down2){
+void PlayerP::controlMove(int scale, int key_up, int key_down,int key_up2, int key_down2){
 
 	if(key_up	|| key_up2) y -= scale*2;//ch='w';
 	if(key_down || key_down2) y += scale*2;//ch='s';
-	LockLimit(scale);
+	this->lockLimit(scale);
 
 }
 
-void playerP::GiveBonus(int bonus_type){
+void PlayerP::giveBonus(int bonus_type){
 
 	if(bonus_type == BONUS_LONG){
 		medlen += 7;
--- src/classes.h
+++ src/classes.h
@@ -1,11 +1,6 @@
 #ifndef CLASSESH
 #define CLASSESH
 
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-
 #define GROSOR  3
 #define MEDLEN  9
 #define RADIUS  6
@@ -16,24 +11,11 @@
 #define BONUS_LONG 1
 #define BONUS_IMPA 2
 #define BONUS_BALL 3
-#define ALPHA_COLOR al_map_rgb(255,0,255)
 
-#ifdef __APPLE__
-#define LONG_DIR "../Resources/long.bmp"
-#define BALL_DIR "../Resources/ball.bmp"
-#define LOGO_DIR "../Resources/pong.bmp"
-#define FONT_DIR "../Resources/font.ttf"
-
-#elif defined _WIN32 || defined _WIN64
-#define LONG_DIR "resources/long.bmp"
-#define BALL_DIR "resources/ball.bmp"
-#define LOGO_DIR "resources/pong.bmp"
-#define FONT_DIR "resources/font.ttf"
-#endif
-
-
-class playerP;
-class element;
+class PlayerP;
+class Element;
+class Ball;
+class Bonus;
 
 class PongGame{
 
@@ -47,12 +29,14 @@ public:
 
     bool paused = false, finished = false;
 
-    playerP* players[2];
+    PlayerP* players[2];
 
-	element *ball, *bonus[2];
+	Ball *ball;
+
+    Bonus *bonus[2];
 
     
-    PongGame(float sc, ALLEGRO_FONT* font);
+    PongGame(float sc);
 
     void restart();
 
@@ -60,57 +44,87 @@ public:
 
     void iniciarPunto(int first);
 
-    void giveScore(playerP* pl, int score);
+    void giveScore(PlayerP* pl, int score);
 
     void processTick(bool* keys);
 
 };
 
-class element{
-	
-	ALLEGRO_BITMAP* sprite;
+
+class Element{
+
+public:
+
+    int stat;
 
     float x, y, x00, y00, radius;
-    int stat, bonus_type;
+    
     float vX, vY, t;
-    bool isCircle;
 
-    public:
+    PongGame *game;
 
-    element(bool iscircle, int bonus, const char *filename);
-    void SetParameters(float px, float py, float vx, float vy,int st = 1); //Configures to start a new movement
-    void ProcessColliding(int scale, int plyrNum, playerP* players[]);
-    void Process(int scale, int plyrNum, playerP* players[]);//Moves, tests collinding and draws the element
-    int GetStat(){ return stat; }
-    void SetStat(int st){ stat = st; }
-    int GetX(){ return x; }
-    int GetY(){ return y; }
-    void SetPos(int px, int py){x = x00 = px; y = y00 = py;}
-    float GetvX(){ return vX; }
-    void Draw(int scale);
+    Element(PongGame *game);
+    void setParameters(float px, float py, float vx, float vy, int st = 1); //Configures to start a new movement
+    void processColliding(int scale, int plyrNum, PlayerP* players[]);
+    virtual void preprocess(){}
+    void process(int scale, int plyrNum, PlayerP* players[]);//Moves, tests collinding and draws the element
+    int getStat(){ return stat; }
+    void setStat(int st){ stat = st; }
+    int getX(){ return x; }
+    int getY(){ return y; }
+    void setPos(int px, int py){x = x00 = px; y = y00 = py;}
+    float getvX(){ return vX; }
+    
+    virtual void playerHit(PlayerP *pl){}
 };
 
 
-class playerP{
+class Ball: public Element{
+	
+public:
 
-    ALLEGRO_FONT* font;
-    int x, y, comTxtY;
-    char comTxt[30];
+    Ball(PongGame* game);
 
+    void playerHit(PlayerP *pl);
+    
+    void preprocess();
+
+};
+
+
+class Bonus: public Element{
+	
     public:
 
-    int score, medlen, bonus_ball, racha;
+    int bonus_type;
 
-    playerP(int px, int py, ALLEGRO_FONT* _font);
-    void GiveBonus(int bonus_type);
-    void Draw(int scale);
-    void LockLimit(int scale);
-    void ControlMove(int scale, int key_up, int key_down, int key_up2 = 0, int key_down2 = 0);
-    void MoveIA(int plyrNum, element *ball, int scale);
-    void SetX(int px){ x = px; }
-    void SetY(int py){ y = py; }
-    int GetX(){ return x; }
-    int GetY(){ return y; }
+    Bonus(PongGame* game, int bonus_type);
+    
+    void playerHit(PlayerP *pl);
+    
+};
+
+
+class PlayerP{
+
+    public:
+    
+    int x, y, comTxtY;
+    char comTxt[30];
+    int score, medlen, racha;
+
+    float bonus_ball;
+
+    PlayerP(int px, int py);
+    void giveBonus(int bonus_type);
+ 
+    void lockLimit(int scale);
+    void controlMove(int scale, int key_up, int key_down, int key_up2 = 0, int key_down2 = 0);
+    void moveIA(int plyrNum, Element *ball, int scale);
+    void setX(int px){ x = px; }
+    void setY(int py){ y = py; }
+    int getX(){ return x; }
+    int getY(){ return y; }
 
 };
 
