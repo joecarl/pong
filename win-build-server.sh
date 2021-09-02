ab411952d6ffc1e02cafe292a72da87924d0bcf7
joecarl <joecarlhr@gmail.com>
2021-09-02 22:18:01 +0200

    refactor: unified win-build script, select server using cli params

--- win-build-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-/c/msys64/msys2_shell.cmd -mingw64 build-server.sh win
--- win-build.sh
+++ win-build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash 
-/c/msys64/msys2_shell.cmd -mingw64 build.sh win
+
+if [ $1 == "server" ] || [ $1 == "s" ]; then
+    echo "[WIN BUILD] COMPILING SERVER"
+	/c/msys64/msys2_shell.cmd -mingw64 build-server.sh win
+else
+    echo "[WIN BUILD] COMPILING CLIENT"
+	/c/msys64/msys2_shell.cmd -mingw64 build.sh win
+fi
+
