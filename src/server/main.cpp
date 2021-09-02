565ddf3ba05b4bc79f912c4e98453c0e46bf91a0
joecarl <joecarlhr@gmail.com>
2021-09-02 13:04:40 +0200

    impl: implemented server working main script

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,13 +2,17 @@
 #include <iostream>
 #include <stdexcept>
 
+#include "server.h"
+
 using namespace std;
 
 int main(int argc, char **argv){
 
 	try{
 		
-		
+		Server server(28090);
+
+		server.run();
 		
 		return 0;
 
