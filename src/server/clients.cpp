36c1168978f894a02c2c2bfc22d67e21aeddb666
joecarl <joecarlhr@gmail.com>
2021-09-02 16:04:53 +0200

    refactor: first adaptation approach server, still working draft

--- src/server/clients.cpp
+++ src/server/clients.cpp
@@ -1,10 +1,3 @@
-//
-//  clients.cpp
-//  mysql_test
-//
-//  Created by Joe on 10/9/18.
-//  Copyright © 2018 Joe. All rights reserved.
-//
 
 #include "clients.h"
 #include "server.h"
@@ -20,28 +13,25 @@ using namespace boost::asio::ip;
 
 int Client::count_instances = 0;
 
-Client::Client(boost::asio::io_service& io_service, tcp::acceptor& acceptor): 
-    socket(io_service)//, 
-    //pl()
+Client::Client(
+	boost::asio::io_service& io_service, 
+	tcp::acceptor& acceptor
+): 
+    socket(io_service)
 {
-
-	//world::players[world::playersQuant] = &pl;
-	//world::playersQuant ++;
-
-	for(int i = 0; i < 1000/*ALLEGRO_KEY_MAX*/; i++) keyb[i] = false;
-	for(int i = 0; i < 10; i++) mouse[i] = false;
 
     this->id_client = count_instances++;
 	acceptor.accept(socket, this->conn_err);
+
 	if(this->conn_err){
 		this->dead = true;
 		cout << "Error opening socket: " << this->conn_err << endl;
-	}
-	else{
+	} else {
 		this->connected = true;
 	}
-	//this->socket = socket_;
+
 }
+
 
 Client::~Client(){
 
@@ -51,60 +41,37 @@ Client::~Client(){
 
 }
 
+void Client::setPlayer(PlayerP * _pl){
+	
+	this->pl = _pl;
+
+}
+
 
 void Client::process_request(std::string request){
 	//let's parse query
 
 	boost::json::value q = boost::json::parse( request );
 	if ( !q.is_object() ){
-		cerr << "Error parsing the request" << endl;
+		cerr << "Not valid JSON" << endl;
 		cerr << request << endl;
 		return;
 	} else {
 		//cout << q << endl;
 	}
     
-    boost::json::object obj = q.get_object();
+    boost::json::object evt = q.get_object();
 
-	string action = obj["action"].get_string().c_str();
-	if(action == "ping"){
-		this->qsend("{\"response\":\"ping\", \"result\":\"ok\"}");
+	auto evtType = evt["type"].get_string();
+	if(evtType == "ping"){
+		evt["type"] = "pong";
+		this->qsend(boost::json::serialize(evt));
+	}
+	else if(evtType == "set_control_state"){
+		cout << evt << endl;
+		this->qsend(boost::json::serialize(evt));
 	}
     
-    /*
-	else if(action == "mouse_move"){
-		int inc_mouse_x = atoi(getstr(q, "dx").c_str());
-		int inc_mouse_y = atoi(getstr(q, "dy").c_str());
-		pl.process_mouse_move(inc_mouse_x, inc_mouse_y);
-	}
-
-	else if(action == "mouseup"){
-		int btn = atoi(getstr(q, "key").c_str());
-		this->mouse[btn] = false;
-	}
-
-	else if(action == "mousedown"){
-		int btn = atoi(getstr(q, "key").c_str());
-		this->mouse[btn] = true;
-	}
-*/
-	else if(action == "keyup"){
-		int keycode = obj["key"].get_int64();//atoi(getstr(q, "key").c_str());
-		this->keyb[keycode] = false;
-	}
-
-	else if(action == "keydown"){
-		int keycode = obj["key"].get_int64();//atoi(getstr(q, "key").c_str());
-		this->keyb[keycode] = true;
-	}
-
-	else{
-		//this->actions.push(q);
-		this->process_stage_action(obj);
-	}
-
-	//this->process_actions_fn(q, this);
-
 }
 /*
 void Client::process_stage_action(boost::json::object& pt){
@@ -116,31 +83,40 @@ void Client::process_stage_action(boost::json::object& pt){
 	}
 }
 */
+
 void Client::qsend(std::string pkg, bool ignore_busy){
+
 	if(this->busy && !ignore_busy){
 		this->pkg_queue.push(pkg);
 		return;
 	}
+
 	//cout << "sending " << pkg.length() << " bytes" << endl;
 	this->busy = true;
+
 	auto handler = boost::bind(&Client::handle_qsent_content, this,
 							   boost::asio::placeholders::error(),
 							   boost::asio::placeholders::bytes_transferred());
-	//boost::asio::write(socket, boost::asio::buffer(pkg + "\r\n\r\n"));
+
 	if(pkg.length() > 1024 * 64){ //max 64kB (me lo estoy inventando a ver si cuela...)
+
 		this->pending_data = pkg.substr (1024 * 64);
 		pkg = pkg.substr (0, 1024 * 64);
 
 	} else {
+
 		this->pending_data = "";
 		pkg = pkg + "\r\n\r\n";
+
 	}
+
 	boost::asio::async_write(socket, boost::asio::buffer(pkg), handler);
-	//socket.async_write_some(boost::asio::buffer(pkg + "\r\n\r\n"), handler);
+
 	this->pkgs_sent++;
 	//std::cout << " S:" << pkgs_sent << endl;
 
 }
+
 
 void Client::handle_qsent_content( const boost::system::error_code& error, std::size_t bytes_transferred){
 
@@ -164,6 +140,28 @@ void Client::handle_qsent_content( const boost::system::error_code& error, std::
 	}
 }
 
+
+bool Client::is_dead(){
+	//return !this->socket.is_open();
+	return this->dead;
+}
+
+
+bool Client::is_connected(){
+	return this->connected;
+}
+
+
+void Client::async_wait_for_data(){
+
+	auto handler = boost::bind(&Client::handle_read_content, this,
+								boost::asio::placeholders::error(),
+								boost::asio::placeholders::bytes_transferred());
+
+	this->socket.async_read_some( boost::asio::buffer(read_buffer, 1024), handler);
+	//cout << "Waiting for data ... " << endl;
+
+}
 
 
 void Client::handle_read_content(const boost::system::error_code& error, std::size_t bytes_transferred){
@@ -192,26 +190,4 @@ void Client::handle_read_content(const boost::system::error_code& error, std::si
 
 	this->async_wait_for_data();
 
-}
-
-bool Client::is_dead(){
-	//return !this->socket.is_open();
-	return this->dead;
-}
-
-bool Client::is_connected(){
-	return this->connected;
-}
-
-void Client::async_wait_for_data(){
-
-	auto handler = boost::bind(&Client::handle_read_content, this,
-								boost::asio::placeholders::error(),
-								boost::asio::placeholders::bytes_transferred());
-
-	this->socket.async_read_some( boost::asio::buffer(read_buffer, 1024), handler);
-	//cout << "Waiting for data ... " << endl;
-
-	//std::string buff = "ADDFSFGDSGSDF GSDFG SDFGDSFGSDFGSDFGSD ";
-	//this->socket.async_write_some(boost::asio::buffer(buff), handler);
 }
--- src/server/clients.h
+++ src/server/clients.h
@@ -1,13 +1,8 @@
-//
-//  clients.hpp
-//  mysql_test
-//
-//  Created by Joe on 10/9/18.
-//  Copyright © 2018 Joe. All rights reserved.
-//
 
 #ifndef clients_hpp
 #define clients_hpp
+
+#include "../classes.h"
 
 #include <boost/json.hpp>
 #include <boost/asio.hpp>
@@ -15,9 +10,11 @@
 #include <queue>
 
 class Client{
+
 	//game vars
 	int pkgs_recv = 0;
 	int pkgs_sent = 0;
+
 	//conn vars
 	std::queue<std::string> pkg_queue;
 	std::string pending_data = "";
@@ -36,17 +33,12 @@ class Client{
 	void handle_read_content( const boost::system::error_code& error, std::size_t bytes_transferred);
 	void handle_qsent_content( const boost::system::error_code& error, std::size_t bytes_transferred);
 
-	void process_stage_action(boost::json::object& pt);
 
 public:
+
+	PlayerP *pl = nullptr;
 	
 	int stage = 0;
-	//std::queue<boost::property_tree::ptree> actions;
-
-
-	//game vars
-    bool keyb[1000];//GLFW_KEY_MAX?
-	bool mouse[10];
 
 	//player pl;
 	bool logged = false;
@@ -55,6 +47,8 @@ public:
 	boost::system::error_code conn_err;
 	Client(boost::asio::io_service& io_service, boost::asio::ip::tcp::acceptor& acceptor);
 	~Client();
+
+	void setPlayer(PlayerP * pl);
 
 	void async_wait_for_data();
 	void qsend(std::string pkg, bool ignore_busy = false);
