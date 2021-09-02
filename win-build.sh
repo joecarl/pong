2567038b78f6210e838dd02545ceeda4583e6639
joecarl <joecarlhr@gmail.com>
2021-09-02 22:55:05 +0200

    fix: fixed warning when no params received

--- win-build.sh
+++ win-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-if [ $1 == "server" ] || [ $1 == "s" ]; then
+if [ "$1" == "server" ] || [ "$1" == "s" ]; then
     echo "[WIN BUILD] COMPILING SERVER"
 	/c/msys64/msys2_shell.cmd -mingw64 build-server.sh win
 else
