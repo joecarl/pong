5f6e12f2b60f5ff099a311a4d2cab27f6face2c7
joecarl <joecarlhr@gmail.com>
2021-09-03 13:43:16 +0200

    style: fixed spaces indent

--- win-build.sh
+++ win-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 if [ "$1" == "server" ] || [ "$1" == "s" ]; then
-    echo "[WIN BUILD] COMPILING SERVER"
+	echo "[WIN BUILD] COMPILING SERVER"
 	/c/msys64/msys2_shell.cmd -mingw64 build-server.sh win
 else
-    echo "[WIN BUILD] COMPILING CLIENT"
+	echo "[WIN BUILD] COMPILING CLIENT"
 	/c/msys64/msys2_shell.cmd -mingw64 build.sh win
 fi
 
