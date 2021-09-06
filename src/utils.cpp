a3b95755601406005445d864623aaf306d8e677b
joecarl <joecarlhr@gmail.com>
2021-09-07 00:30:15 +0200

    style: indenting with tabs

--- src/utils.cpp
+++ src/utils.cpp
@@ -52,11 +52,11 @@ std::string file_get_contents(std::string filepath){
 
 bool file_exists(const std::string& name) {
 
-    if (FILE *file = fopen(name.c_str(), "r")) {
-        fclose(file);
-        return true;
-    } else {
-        return false;
-    }   
+	if (FILE *file = fopen(name.c_str(), "r")) {
+		fclose(file);
+		return true;
+	} else {
+		return false;
+	}
 
 }
