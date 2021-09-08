5bca74952ecb28d979246fc4a2a39198bdb1df7b
joecarl <joecarlhr@gmail.com>
2021-09-08 12:41:17 +0200

    feat: added exec and trim functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,16 @@
 
 #include "utils.h"
 
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 #include <fstream>
+#include <algorithm> 
+#include <cctype>
+#include <locale>
 
 using namespace std;
 
@@ -58,5 +67,58 @@ bool file_exists(const std::string& name) {
 	} else {
 		return false;
 	}
+
+}
+
+
+string exec(const char* cmd) {
+
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
+    if (!pipe) {
+        throw runtime_error("popen() failed!");
+    }
+
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+
+    return result;
+
+}
+
+
+
+// trim from start (in place)
+string ltrim(string s) {
+
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+
+	return s;
+
+}
+
+// trim from end (in place)
+string rtrim(string s) {
+
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+
+	return s;
+
+}
+
+// trim from both ends (in place)
+string trim(string s) {
+
+    s = ltrim(s);
+    s = rtrim(s);
+
+	return s;
 
 }
--- src/utils.h
+++ src/utils.h
@@ -11,4 +11,9 @@ std::string extract_pkg(std::string& raw);
 
 bool file_exists(const std::string& name);
 
+std::string exec(const char* cmd);
+
+// trim from both ends
+std::string trim(std::string s);
+
 #endif
