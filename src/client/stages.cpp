f48a299229365ff36341f12e9f9a475fc1a05be4
joecarl <joecarlhr@gmail.com>
2021-09-17 01:15:04 +0200

    removed unnecessary header

--- src/client/stages.cpp
+++ src/client/stages.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
