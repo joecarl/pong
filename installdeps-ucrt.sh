0ec0a45344ac6d7b86867064abba89201ce6c5b9
José Carlos HR <joecarlhr@gmail.com>
2023-03-08 20:25:00 +0100

    add ucrt deps install mode

---
+++ installdeps-ucrt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#global-deps
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-gcc
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-boost
+
+#client-only-deps
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-freetype
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-allegro 
+
+#allegro-deps(most of them will already be installed)
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-freeimage
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-cmake
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-dumb
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-flac
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-libjpeg-turbo
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-libpng
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-libvorbis
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-libwebp
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-openal
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-opusfile
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-physfs
+pacman -S --noconfirm ucrt64/mingw-w64-ucrt-x86_64-pkg-config
