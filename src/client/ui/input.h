bd02b4043ad11c158ed6b8b874e29f25f322d87b
joecarl <joecarlhr@gmail.com>
2023-08-25 20:40:17 +0000

    declare input virtual methods

--- src/client/ui/input.h
+++ src/client/ui/input.h
@@ -1,6 +1,9 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include <boost/json.hpp>
+#include <string>
+
 enum InputType {
 	INPUT_TYPE_TEXT,
 	INPUT_TYPE_SELECT,
@@ -10,7 +13,27 @@ enum InputType {
 
 class Input {
 public:
+
+	std::string label;
+
+	//double x;
+
+	//double y;
+
 	InputType type;
+
+	virtual void draw(float x, float y) = 0;
+
+	virtual void focus() = 0;
+
+	virtual void blur() = 0;
+
+	virtual bool is_focused() = 0;
+
+	virtual void set_from_json_value(boost::json::value& val) = 0;
+
+	virtual boost::json::value get_json_value() = 0;
+
 };
 
 #endif
